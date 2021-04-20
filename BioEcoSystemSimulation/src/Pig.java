f393bcdfaac810f7834636ee1c85bdab3dd18390
Mabel Ling <mabelling42@gmail.com>
2021-04-20 11:18:38 -0700

    Create pig and tiger classes

---
+++ BioEcoSystemSimulation/src/Pig.java
@@ -0,0 +1,12 @@
+public class Pig extends Prey{
+    
+    public Pig(MapGUI mapGUI, int r, int c) {
+        super(mapGUI, r , c);
+    }
+    
+    public Pig(MapGUI mapGUI, int r, int c, int age, int hunger, int thirst, int strength) {
+        super(mapGUI, r, c, age, hunger, thirst, strength);
+    }
+    
+    
+}
---
+++ BioEcoSystemSimulation/src/Tiger.java
@@ -0,0 +1,15 @@
+
+public class Tiger extends Predator {
+    
+    public Tiger(MapGUI mapGUI, int r, int c) {
+        super(mapGUI, r, c);
+        
+    }
+    
+    public Tiger(MapGUI mapGUI, int r, int c, int age, int hunger, int thirst, int strength) {
+        super(mapGUI, r, c, age, hunger, thirst, strength);
+    }
+    
+    
+    
+}
